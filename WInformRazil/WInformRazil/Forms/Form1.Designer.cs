841dc4a11565b8b349d79cf927df997514b99359
Razil2005 <razilcavadov155@gmail.com>
2025-01-18 15:24:00 +0400

    initial change

---
+++ WInformRazil/WInformRazil/Forms/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WInformRazil
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            dataGridView2 = new DataGridView();
+            btnChangeLogin = new Button();
+            lblInformation = new Label();
+            btn_InformationPage = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.GhostWhite;
+            dataGridView1.ColumnHeadersHeight = 29;
+            dataGridView1.Location = new Point(21, 118);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(601, 265);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Thistle;
+            button1.Location = new Point(21, 407);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 34);
+            button1.TabIndex = 1;
+            button1.Text = "Take Book";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Thistle;
+            button2.Location = new Point(672, 407);
+            button2.Name = "button2";
+            button2.Size = new Size(109, 34);
+            button2.TabIndex = 2;
+            button2.Text = "Give Book";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.BackgroundColor = Color.MintCream;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(672, 118);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 51;
+            dataGridView2.Size = new Size(558, 265);
+            dataGridView2.TabIndex = 3;
+            // 
+            // btnChangeLogin
+            // 
+            btnChangeLogin.BackColor = Color.DarkOrchid;
+            btnChangeLogin.ForeColor = SystemColors.ButtonFace;
+            btnChangeLogin.Location = new Point(136, 490);
+            btnChangeLogin.Name = "btnChangeLogin";
+            btnChangeLogin.Size = new Size(278, 50);
+            btnChangeLogin.TabIndex = 4;
+            btnChangeLogin.Text = "Go To Change Login";
+            btnChangeLogin.UseVisualStyleBackColor = false;
+            btnChangeLogin.Click += BtnChangeLogin_Click;
+            // 
+            // lblInformation
+            // 
+            lblInformation.AutoSize = true;
+            lblInformation.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblInformation.Location = new Point(346, 24);
+            lblInformation.Name = "lblInformation";
+            lblInformation.Size = new Size(420, 54);
+            lblInformation.TabIndex = 5;
+            lblInformation.Text = "Library Management";
+            // 
+            // btn_InformationPage
+            // 
+            btn_InformationPage.BackColor = Color.Purple;
+            btn_InformationPage.ForeColor = SystemColors.ButtonHighlight;
+            btn_InformationPage.Location = new Point(656, 490);
+            btn_InformationPage.Name = "btn_InformationPage";
+            btn_InformationPage.Size = new Size(203, 45);
+            btn_InformationPage.TabIndex = 6;
+            btn_InformationPage.Text = "Go To About";
+            btn_InformationPage.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Orchid;
+            ClientSize = new Size(1279, 600);
+            Controls.Add(btn_InformationPage);
+            Controls.Add(lblInformation);
+            Controls.Add(dataGridView2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnChangeLogin);
+            Name = "Form1";
+            Text = "Home";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private DataGridView dataGridView2;
+        private Button btnChangeLogin;
+        private Label lblInformation;
+        private Button btn_InformationPage;
+    }
+}
---
+++ WInformRazil/WInformRazil/Forms/LoginForm.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace WInformRazil.Forms
+{
+    partial class LoginForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Label lblLogin;
+        private TextBox txtLogin;
+        private Label lblPassword;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private ToolTip toolTip;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            lblLogin = new Label();
+            txtLogin = new TextBox();
+            lblPassword = new Label();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            toolTip = new ToolTip(components);
+            btn_SignUp = new Button();
+            lbl_AccountInformation = new Label();
+            checkBox_ShowPassword = new CheckBox();
+            lbl_PasswordInformation = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            lblLogin.AutoSize = true;
+            lblLogin.Location = new Point(562, 138);
+            lblLogin.Name = "lblLogin";
+            lblLogin.Size = new Size(49, 20);
+            lblLogin.TabIndex = 0;
+            lblLogin.Text = "Login:";
+            // 
+            // txtLogin
+            // 
+            txtLogin.Location = new Point(644, 135);
+            txtLogin.Name = "txtLogin";
+            txtLogin.Size = new Size(267, 27);
+            txtLogin.TabIndex = 1;
+            toolTip.SetToolTip(txtLogin, "Lütfen kullanıcı adınızı girin");
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(562, 175);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(73, 20);
+            lblPassword.TabIndex = 2;
+            lblPassword.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(644, 172);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(267, 27);
+            txtPassword.TabIndex = 3;
+            toolTip.SetToolTip(txtPassword, "Lütfen şifrenizi girin");
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.PeachPuff;
+            btnLogin.Location = new Point(644, 257);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(190, 43);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Login";
+            toolTip.SetToolTip(btnLogin, "Giriş yap");
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += BtnLogin_Click;
+            // 
+            // btn_SignUp
+            // 
+            btn_SignUp.BackColor = Color.Peru;
+            btn_SignUp.Location = new Point(775, 306);
+            btn_SignUp.Name = "btn_SignUp";
+            btn_SignUp.Size = new Size(105, 33);
+            btn_SignUp.TabIndex = 6;
+            btn_SignUp.Text = "sign up";
+            btn_SignUp.TextAlign = ContentAlignment.TopCenter;
+            btn_SignUp.UseVisualStyleBackColor = false;
+            // 
+            // lbl_AccountInformation
+            // 
+            lbl_AccountInformation.AutoSize = true;
+            lbl_AccountInformation.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl_AccountInformation.ForeColor = Color.Red;
+            lbl_AccountInformation.Location = new Point(562, 306);
+            lbl_AccountInformation.Name = "lbl_AccountInformation";
+            lbl_AccountInformation.Size = new Size(216, 28);
+            lbl_AccountInformation.TabIndex = 7;
+            lbl_AccountInformation.Text = "Don't have an account?";
+            // 
+            // checkBox_ShowPassword
+            // 
+            checkBox_ShowPassword.AutoSize = true;
+            checkBox_ShowPassword.Location = new Point(603, 222);
+            checkBox_ShowPassword.Name = "checkBox_ShowPassword";
+            checkBox_ShowPassword.Size = new Size(129, 24);
+            checkBox_ShowPassword.TabIndex = 8;
+            checkBox_ShowPassword.Text = "Remember me";
+            checkBox_ShowPassword.UseVisualStyleBackColor = true;
+            // 
+            // lbl_PasswordInformation
+            // 
+            lbl_PasswordInformation.AutoSize = true;
+            lbl_PasswordInformation.ForeColor = Color.Red;
+            lbl_PasswordInformation.Location = new Point(763, 222);
+            lbl_PasswordInformation.Name = "lbl_PasswordInformation";
+            lbl_PasswordInformation.Size = new Size(124, 20);
+            lbl_PasswordInformation.TabIndex = 9;
+            lbl_PasswordInformation.Text = "Forget Password?";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(717, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 10;
+            label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(0, 1);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(498, 374);
+            pictureBox1.TabIndex = 11;
+            pictureBox1.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Orange;
+            ClientSize = new Size(958, 376);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(lbl_PasswordInformation);
+            Controls.Add(checkBox_ShowPassword);
+            Controls.Add(lbl_AccountInformation);
+            Controls.Add(btn_SignUp);
+            Controls.Add(lblLogin);
+            Controls.Add(txtLogin);
+            Controls.Add(lblPassword);
+            Controls.Add(txtPassword);
+            Controls.Add(btnLogin);
+            Name = "LoginForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            Load += LoginForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string login = txtLogin.Text;
+            string password = txtPassword.Text;
+
+            if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Login and Password cannot be empty!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+
+            bool isAuthenticated = WInformRazil.Services.LoginService.Authenticate(login, password);
+
+            if (isAuthenticated)
+            {
+                MessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Form1 mainForm = new Form1();
+                mainForm.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid login credentials.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private Button btn_SignUp;
+        private Label lbl_AccountInformation;
+        private CheckBox checkBox_ShowPassword;
+        private Label lbl_PasswordInformation;
+        private Label label1;
+        private PictureBox pictureBox1;
+    }
+}
